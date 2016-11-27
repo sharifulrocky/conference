672631cfdeb460f19dfd3d27a9ec215f1a56c9ba
shariful <rocky.cse09@gmail.com>
2016-11-27 19:02:13 +0600

    Static file directory, templates directory added and sample test with index.html

--- conference/urls.py
+++ conference/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from app.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^test', index, name='index')
 ]
