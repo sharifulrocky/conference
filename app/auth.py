11ebb587db655d1e2dc1ed73c7687555a05b6fc0
shariful <rocky.cse09@gmail.com>
2016-11-28 00:17:24 +0600

    login, logout and menu view updated

--- app/auth.py
+++ app/auth.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import auth
 
 
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
 def login(request):
 
     if request.method == "GET":
--- app/views.py
+++ app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'master.html')
+    return render(request, 'index.html')
